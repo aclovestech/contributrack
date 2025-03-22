8de207f56c38c56023825fab59ab39cca097ee16
aclovestech <48392273+aclovestech@users.noreply.github.com>
2025-03-21 17:56:02 -0700

    refactor(tanstack-query): move QueryClientProvider to main.tsx

--- packages/contributrack-frontend/src/routes/__root.tsx
+++ packages/contributrack-frontend/src/routes/__root.tsx
@@ -1,19 +1,12 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ThemeProvider } from '../components/theme-provider';
-
-const queryClient = new QueryClient();
 
 export const Route = createRootRoute({
   component: () => (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <QueryClientProvider client={queryClient}>
-        <Outlet />
-        <TanStackRouterDevtools />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
+      <Outlet />
+      <ReactQueryDevtools initialIsOpen={false} />
     </ThemeProvider>
   ),
 });
