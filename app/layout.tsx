95cc1f7e3a21b3729e8e815c85b930e8d651fb6d
aclovestech <48392273+aclovestech@users.noreply.github.com>
2025-03-22 19:00:06 -0700

    feat(stack-auth): set up stack auth for the project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from 'next';
+import { StackProvider, StackTheme } from "@stackframe/stack";
+import { stackServerApp } from "../stack";
 import { Inter } from 'next/font/google';
 import './globals.css';
 
@@ -16,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}><StackProvider app={stackServerApp}><StackTheme>{children}</StackTheme></StackProvider></body>
     </html>
   );
 }
