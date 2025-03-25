92e04bf5e957223f6791b251c3006a9994ba6162
aclovestech <48392273+aclovestech@users.noreply.github.com>
2025-03-24 17:17:36 -0700

    feat(webhook): implement StackAuth webhook handling for user events to manage user data in the database feat(database): create initial SQL schema for donations and donors tables to support donation tracking chore(package): remove dotenv dependency and update svix package for webhook support refactor(db): simplify database connection setup by directly using DATABASE_URL

---
+++ app/api/webhook/route.ts
@@ -0,0 +1,50 @@
+import { Webhook } from 'svix';
+import { StackAuthWebhookData } from './StackAuthWebhookData';
+import { usersTable } from '@/src/db/schema';
+import { db } from '@/src/db';
+import { eq } from 'drizzle-orm';
+
+const secret = process.env.STACK_AUTH_WEBHOOK_SECRET!;
+
+export async function POST(req: Request) {
+  // get the needed headers from the request
+  const headers = {
+    'svix-id': req.headers.get('svix-id') ?? '',
+    'svix-timestamp': req.headers.get('svix-timestamp') ?? '',
+    'svix-signature': req.headers.get('svix-signature') ?? '',
+  };
+
+  // get the payload
+  const payload = await req.text();
+
+  // create a new Webhook
+  const sivx = new Webhook(secret);
+
+  let msg;
+
+  try {
+    // verify the webhook
+    msg = sivx.verify(payload, headers);
+
+    // get the data from the webhook
+    const { data, type: eventType } = (await JSON.parse(
+      payload
+    )) as StackAuthWebhookData;
+
+    // if the event type is user.created, insert the user into the database
+    if (eventType === 'user.created') {
+      await db.insert(usersTable).values({
+        id: data.id,
+      });
+    }
+    // if the event type is user.deleted, delete the user from the database
+    else if (eventType === 'user.deleted') {
+      await db.delete(usersTable).where(eq(usersTable.id, data.id));
+    }
+  } catch (error) {
+    console.error(error);
+    return new Response(`Error processing webhook: ${error}`, { status: 400 });
+  }
+
+  return new Response('Webhook received', { status: 200 });
+}
