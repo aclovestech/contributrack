5b4a13175ab02fc9b56d972c1ba0f3259b351708
aclovestech <48392273+aclovestech@users.noreply.github.com>
2025-03-30 22:29:17 -0700

    refactor(components): convert components to client-side
    - convert data-table, add-donor-dialog, donor-selector to client components
    - use useState hook instead of React.useState
    - remove unused Button import from site-header
    - rename Home page to RootPage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { stackServerApp } from '@/stack';
 import { redirect } from 'next/navigation';
 
-export default async function Home() {
+export default async function RootPage() {
   const user = await stackServerApp.getUser({ or: 'redirect' });
 
   if (user) {
