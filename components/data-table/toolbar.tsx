489f3479c0006459e7f0e7bce1ebd56534f3c01d
aclovestech <48392273+aclovestech@users.noreply.github.com>
2025-04-05 16:26:07 -0700

    feat(data-table): enhance data table with global filtering
    - implement global filtering functionality for data tables
    - add input field for filtering across all columns
    - include reset button to clear the filter
    - integrate `@tanstack/match-sorter-utils` for fuzzy matching
    - remove column specific filtering

--- components/data-table/toolbar.tsx
+++ components/data-table/toolbar.tsx
@@ -22,54 +22,36 @@ export function DataTableToolbar<TData>({
 
   const currentPage = usePathname().split('/')[2];
 
-  let columnToFilter: string | undefined;
-  let placeholder;
+  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setFilter(String(event.target.value));
+    table.setGlobalFilter(String(event.target.value));
+  }
 
-  switch (currentPage) {
-    case 'donors': {
-      columnToFilter = 'name';
-      placeholder = 'name';
-      break;
-    }
-    case 'donations': {
-      columnToFilter = 'donorName';
-      placeholder = 'donor name';
-      break;
-    }
-    case 'reports': {
-      break;
-    }
+  function handleResetFilter() {
+    setFilter('');
+    table.resetGlobalFilter();
   }
 
   return (
     <div className="flex items-center justify-between">
-      {columnToFilter && (
-        <div className="flex flex-1 items-center space-x-2">
-          <Input
-            placeholder={`Filter ${placeholder}`}
-            value={
-              (table.getColumn(columnToFilter)?.getFilterValue() as string) ??
-              ''
-            }
-            onChange={(event) =>
-              table
-                .getColumn(columnToFilter)
-                ?.setFilterValue(event.target.value)
-            }
-            className="h-8 w-[150px] lg:w-[250px]"
-          />
-          {isFiltered && (
-            <Button
-              variant="ghost"
-              onClick={() => table.resetColumnFilters()}
-              className="h-8 px-2 lg:px-3"
-            >
-              Reset
-              <X />
-            </Button>
-          )}
-        </div>
-      )}
+      <div className="flex flex-1 items-center space-x-2">
+        <Input
+          placeholder="Filter table..."
+          value={filter}
+          onChange={handleInputChange}
+          className="h-8 w-[150px] lg:w-[250px]"
+        />
+        {isFiltered && (
+          <Button
+            variant="ghost"
+            onClick={handleResetFilter}
+            className="h-8 px-2 lg:px-3"
+          >
+            Reset
+            <X />
+          </Button>
+        )}
+      </div>
       <div className="flex items-center space-x-2">
         {currentPage !== 'donations' && <DataTableViewOptions table={table} />}
         {currentPage === 'donors' && <AddDonorDialog />}
