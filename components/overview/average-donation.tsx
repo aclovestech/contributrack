d8d8c1edc2ff2bb8f3b44d92c2c1c826c7871189
aclovestech <48392273+aclovestech@users.noreply.github.com>
2025-04-04 20:07:24 -0700

    feat(donation): display average donation amount
    - add function to calculate average donation amount
    - display average donation amount in overview page

--- components/overview/average-donation.tsx
+++ components/overview/average-donation.tsx
@@ -7,26 +7,61 @@ import {
   CardTitle,
 } from '@/components/ui/card';
 import { Badge } from '@/components/ui/badge';
-import { IconTrendingUp } from '@tabler/icons-react';
+import { IconTrendingDown, IconTrendingUp } from '@tabler/icons-react';
+import { stackServerApp } from '@/stack';
+import { getAverageDonationYtd } from '@/actions/donations.action';
+import { calculateTrend } from '@/lib/utils';
 
-export default function AverageDonation() {
+export default async function AverageDonation() {
+  const user = await stackServerApp.getUser({ or: 'redirect' });
+
+  const data = await getAverageDonationYtd(user.id);
+
+  const trend = calculateTrend(data.currentYear, data.previousYear);
+
+  const formattedAverageDonation = data.currentYear.toLocaleString(undefined, {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+
   return (
     <Card className="@container/card">
       <CardHeader>
         <CardDescription>Average Donation</CardDescription>
         <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-          $45,678
+          ${formattedAverageDonation}
         </CardTitle>
         <CardAction>
           <Badge variant="outline">
-            <IconTrendingUp />
-            +12.5%
+            {trend.isPositive ? (
+              <>
+                <IconTrendingUp className="text-green-500" />+{trend.output}
+              </>
+            ) : (
+              <>
+                <IconTrendingDown className="text-red-500" />
+                {trend.output}
+              </>
+            )}
           </Badge>
         </CardAction>
       </CardHeader>
       <CardFooter className="flex-col items-start gap-1.5 text-sm">
         <div className="line-clamp-1 flex gap-2 font-medium">
-          In comparison to overall average
+          {trend.isPositive ? (
+            <>
+              <IconTrendingUp className="size-4 text-green-500" />
+              Trending up this year
+            </>
+          ) : (
+            <>
+              <IconTrendingDown className="size-4 text-red-500" />
+              Trending down this year
+            </>
+          )}
+        </div>
+        <div className="text-muted-foreground">
+          From the beginning of the year to date
         </div>
       </CardFooter>
     </Card>
